51db3f93d0b4c1e0ab7258799d796480e9a409d3
Sangwon Park <sangwonpark@Sangwons-MacBook-Pro.local>
2024-02-12 17:12:19 +0900

    food_detection api 추가

---
+++ food_detection.py
@@ -0,0 +1,68 @@
+import PIL.Image
+import google.generativeai as genai
+from starlette.config import Config
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+config = Config(".env")
+GEMINI_API_KEY = config("GEMINI_API_KEY")
+
+
+@app.get("/")
+def food_detection(img_path: str):
+    # set api key retrieved from .env file GEMINI_API_KEY variable
+    genai.configure(api_key=GEMINI_API_KEY)
+
+    model = genai.GenerativeModel("gemini-pro-vision")
+
+    # TODO : img 어디서 가져올건지? firebase 같은 곳에 올리고 가져오기 ?
+    img = PIL.Image.open(img_path)
+
+    # get food list (e.g. ['apple', 'banana', 'orange'])
+    foods = model.generate_content(
+        [
+            "Extract the food information in the provided image and output them in a list in alphabetical order (if there is no food in provided image, print 'None').",
+            img,
+        ],
+        stream=True,
+    )
+    foods.resolve()
+    foods = (
+        (foods.text)
+        .replace("*", "")
+        .replace("-", " ")
+        .replace("\n", "(split)")
+        .replace(".", "")
+        .split("(split)")
+    )
+    foods = [food.strip() for food in foods]
+
+    # get leftover decision ('Yes' or 'No')
+    leftover_decision = model.generate_content(
+        [
+            "Are there any leftovers in this photo? (Even if food is left uneaten, if there is food, it is treated as leftover food) Let me know with Yes or No whether this person leave any the food or not.",
+            img,
+        ],
+        stream=True,
+    )
+    leftover_decision.resolve()
+    leftover_decision = (leftover_decision.text).replace(".", "")
+    leftover_decision = leftover_decision.strip()
+
+    return foods, leftover_decision
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--img_path", type=str)
+    parser.add_argument("--api_key", type=str)
+
+    args = parser.parse_args()
+
+    foods, leftover_decision = food_detection(args.img_path, args.api_key)
+
+    print(f"foods: {foods} \nleftover_decision: {leftover_decision}")
